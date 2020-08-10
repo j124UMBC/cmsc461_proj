6bd3885f08cf5a83a4173412016073eaae34c7c1
meowkun <ktao1@umbc.edu>
2020-08-10 13:37:47 -0400

    Update app.py

--- application_level/app.py
+++ application_level/app.py
@@ -6,8 +6,6 @@ import sys
 from sys import platform
 from os import system, path
 
-#READ DATA FROM CSV FILE INTO DATABASE
-###
 #insert contents of csv file into the office table
 def read_office(con, cur, file_name):
     with open(file_name, 'r') as office:
@@ -48,107 +46,106 @@ def read_customer_agency(con, cur, file_name):
             non_records += 1
     print('*** {} Records transferred to customer_agency! ***'.format(non_records))
 
-def clean():
-    #init db connection
-    con = lite.connect('../physical_level/GSA.db')
-    cur = con.cursor()
-    
-    #clear all tables
-    cur.execute("DELETE FROM office")
-    cur.execute("DELETE FROM parties")
-    cur.execute("DELETE FROM customer_agency")
-    cur.execute("DELETE FROM rental_agreement")
-
-    #close db 
-    con.commit()
-    con.close()
-
-#READ SQL COMMANDS FROM USER INPUT
 def main():
+    #clear terminal
+    if platform == "linux" or platform == "darwin":
+        system("clear");
+    elif platform == "win32":
+        system("cls");
+
+    #intro
     print('SQL COMMAND SHELL');
     print('-----------------------------------------------');
     con = None
 
+    #open db
+    try:
+        con = lite.connect('../physical_level/GSA.db')
+        cur = con.cursor()
+    except lite.Error as e:
+        print("Error %s:" % e.args[0])
+        sys.exit(1)
+
+    #1 for sql command, 2 to add custom csv, 3 clear all tables of data, 4 to quit app
     loop_control = 0
-    #1 for sql command, 2 to add custom csv, 3 to quit app
     while(loop_control != '4'):
         print('1. Run SQL commands\n2. Load new csv file\n3. Clean database\n4. Quit')
         loop_control = input('What would you like to do: ')
         while(loop_control != '1' and loop_control != '2' and loop_control != '3' and loop_control != '4'):
             loop_control = input('Try again: ')
 
-        if(loop_control == '1'):
-            #init db connection
-            try:
-                con = lite.connect('../physical_level/GSA.db')
-                cur = con.cursor()
-            except lite.Error as e:
-                    print("Error %s:" % e.args[0])
-            
-            loop_control = "y"
-            while loop_control != "n":
-                #clear terminal
-                try:
-                    if platform == "linux" or platform == "darwin":
-                        system("clear");
-                    elif platform == "win32":
-                        system("cls");
+        try:
+            if(loop_control == '1'):
+                run(con, cur)
+            if(loop_control == '2'):
+                load(con, cur)
+            if loop_control == '3':
+                clean(con, cur)
 
-                    #get SQL comand from user input
-                    myStmt = input('Enter SQL command: ')
-                    cur.execute(myStmt)
+        except lite.Error as e:
+            print("Error %s:" % e.args[0])
+            sys.exit(1)
 
-                    #display results
-                    data = cur.fetchall()
-                    print("The results are: ")
-                    for record in data:
-                        print(record)
-                    loop_control = input('Enter another command? (y/n): ')
+    # close db
+    con.close()
 
+def run(con, cur):
+    loop_control = "y"
+    while loop_control != "n":
+        #clear terminal
+        if platform == "linux" or platform == "darwin":
+            system("clear");
+        elif platform == "win32":
+            system("cls");
 
-                except lite.Error as e:
-                    print("Error %s:" % e.args[0])
-            #commit transaction and close the db connection
-            con.commit()
-            con.close()
+        #get SQL comand from user input
+        myStmt = input('Enter SQL command: ')
+        cur.execute(myStmt)
+        con.commit()
 
-            #clear terminal
-            if platform == "linux" or platform == "darwin":
-                system("clear");
-            elif platform == "win32":
-                system("cls");
+        #display results
+        data = cur.fetchall()
+        print("The results are: ")
+        for record in data:
+            print(record)
+        loop_control = input('Enter another command? (y/n): ')
 
-        if(loop_control == '2'):
-            #init db connection
-            con = lite.connect('../physical_level/GSA.db')
-            cur = con.cursor()
+    #clear terminal
+    if platform == "linux" or platform == "darwin":
+        system("clear");
+    elif platform == "win32":
+        system("cls");
 
-            print('1. Office\n2. Parties\n3. Rental agreement\n4. Customer agency')
-            table_choice = input('Choose a table to insert data into: ')
-            while(table_choice != '1' and table_choice != '2' and table_choice != '3' and table_choice != '4'):
-                table_choice = input('Try again: ')
+def load(con, cur):
+    print('1. Office\n2. Parties\n3. Rental agreement\n4. Customer agency')
+    table_choice = input('Choose a table to insert data into: ')
+    while(table_choice != '1' and table_choice != '2' and table_choice != '3' and table_choice != '4'):
+        table_choice = input('Try again: ')
 
-            file_name = input('Please enter the file path to the csv file you would like to use (i.e. ./<file>.csv): ')
-            while(os.path.exists(file_name) == False):
-                file_name = input('Try again: ')
+    file_name = input('Please enter the file path to the csv file you would like to use (i.e. ./<file>.csv): ')
+    while(os.path.exists(file_name) == False):
+        file_name = input('Try again: ')
 
-            if(table_choice == '1'):
-                read_office(con, cur, file_name)
+    if(table_choice == '1'):
+        read_office(con, cur, file_name)
 
-            if(table_choice == '2'):
-                read_parties(con, cur, file_name)
+    if(table_choice == '2'):
+        read_parties(con, cur, file_name)
 
-            if(table_choice == '3'):
-                read_rental_agreement(con, cur, file_name)
+    if(table_choice == '3'):
+        read_rental_agreement(con, cur, file_name)
 
-            if(table_choice == '4'):
-                read_customer_agency(con, cur, file_name)
+    if(table_choice == '4'):
+        read_customer_agency(con, cur, file_name)
 
-            con.commit()
-            con.close()
+def clean(con, cur):
+    #clear all tables
+    cur.execute("DELETE FROM office")
+    cur.execute("DELETE FROM parties")
+    cur.execute("DELETE FROM customer_agency")
+    cur.execute("DELETE FROM rental_agreement")
+    con.commit()
 
-        if loop_control == '3':
-            clean()
 
 if __name__ == "__main__":
     main()
